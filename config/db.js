c225e1fd5dcbd8aadc3333a9d3393bfe163651c5
gkumar-pandey <gautamshekhar078@gmail.com>
2024-03-14 13:08:45 +0530

    connected to database

--- config/db.js
+++ config/db.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect();
+    const DB_USERNAME = process.env.DB_USERNAME;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
+    const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.nmh3pcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const connect = await mongoose.connect(DB_URL);
     console.log("Database connected successfully..");
   } catch (error) {
     console.error(error);
-  } finally {
-    mongoose.disconnect();
-  }
+    
+  }  
 };
 
 module.exports = connectDb;
--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { routeNotFound } = require("./middleware");
 const routes = require("./routes");
+const connectDb = require("./config/db");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT | 3000;
+connectDb()
 
 app.use("/", routes);
 app.use(routeNotFound);
