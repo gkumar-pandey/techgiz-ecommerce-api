6a7e0275e2131eba587600eb5b9f3df0721e0d05
gkumar-pandey <gautamshekhar078@gmail.com>
2024-10-21 23:24:41 +0530

    changed the db connection url

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,7 +4,7 @@ const connectDb = async () => {
   try {
     const DB_USERNAME = process.env.DB_USERNAME;
     const DB_PASSWORD = process.env.DB_PASSWORD;
-    const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.nmh3pcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@gautam-db.ze7ri.mongodb.net/?retryWrites=true&w=majority&appName=Gautam-DB`;
     const connect = await mongoose.connect(DB_URL);
     if (connect) {
       console.log('MongoDB connected successfully');
